7f2e1912b235cd974f85b091d3286e638ff245da
tocsinDE <hallo@tocsin.de>
2015-11-10 17:11:45 +0100

    added better version of DataInitializer

--- prototypes/prototype1/src/main/java/kickstart/KickstartDataInitializer.java
+++ prototypes/prototype1/src/main/java/kickstart/KickstartDataInitializer.java
@@ -25,22 +25,16 @@ import org.salespointframework.useraccount.UserAccountManager;
 		UserAccount admin = userAccountManager.create("admin", "123", new Role("ROLE_BOSS"));
 		userAccountManager.save(admin);
 		
-		
-		// hier müssten noch Mitarbeiter rein
-		// zu klären: welche ROLE wird denen zugeordnet? Kevin: bitte ROLE_EMPLOYEE verwenden
-		// Recherche-Aufgabe: Wie werden Mitarbeiter in Salespoint festgelegt? Kevin: würde ich auch mit dem User machen
-		// sind ja Mittelding zwischen Kunden und Admin
-		
-		
-		UserAccount  = userAccountManager.create("admin", "123", new Role("ROLE_BOSS"));
+		admin  = userAccountManager.create("admin", "123", new Role("ROLE_BOSS"));
 		userAccountManager.save(admin);
 		
-		// Achja: hier muss noch zwischen Privat- und Geschäftskunden unterschieden werden
-		// extra ROLE namens ROLE_NORMALCUSTOMER? // ROLE_PCUSTOMER
-		// extra ROLE namens ROLE_ENTERPRISECUSTOMER? ROLE_BCUSTOMER (buissiness)
+		
+		employee  = userAccountManager.create("employee1", "123", new Role("ROLE_EMPLOYEE"));
+		userAccountManager.save(employee);
+	
 		
 		
-		final Role customerRole = new Role("ROLE_PCUSTOMER"); 
+		final Role customerRole = new Role("ROLE_BCUSTOMER"); 
 		
 		// hier wird erstmal allgemein ein Account auf Salespoint-Basis erstellt
 		UserAccount ua1 = userAccountManager.create("haensel", "123", customerRole);
@@ -49,11 +43,25 @@ import org.salespointframework.useraccount.UserAccountManager;
 		userAccountManager.save(ua2);
 		
 		
-		
-		
-		// hier werden zusätzliche Daten für die Kunden ergänzt
+		// hier werden zusätzliche Daten für die GESCHÄFTSKunden ergänzt
 		Customer c1 = new Customer(ua1, "Straße 1", "Hänsel", "Nachname", "h@ensel.de", "0800-1234567");
 		Customer c2 = new Customer(ua2, "Straße 2", "Gretel", "Nachname", "gretel@web.de", "0800-7891011");
 		
-		customerRepository.save(Arrays.asList(c1, c2));
+		
+		final Role customerRole = new Role("ROLE_PCUSTOMER"); 
+		
+		// hier wird erstmal allgemein ein Account auf Salespoint-Basis erstellt
+		UserAccount ua3 = userAccountManager.create("haensel", "123", customerRole);
+		userAccountManager.save(ua1);
+		UserAccount ua4 = userAccountManager.create("gretel", "123", customerRole);
+		userAccountManager.save(ua2);
+		
+		
+		// hier werden zusätzliche Daten für die PRIVATKunden ergänzt
+		Customer c3 = new Customer(ua3, "Straße 1", "Hänsel", "Nachname", "h@ensel.de", "0800-1234567");
+		Customer c4 = new Customer(ua4, "Straße 2", "Gretel", "Nachname", "gretel@web.de", "0800-7891011");
+		
+		
+		// alle Kunden (Geschäfts- und Privatkunden) speichern
+		customerRepository.save(Arrays.asList(c1, c2, c3, c4));
 	}
