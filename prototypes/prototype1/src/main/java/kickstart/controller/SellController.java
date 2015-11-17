97e933562c713d3ecf32c02b303ad2c635833a58
s7358731 <philipp-oehme@gmx.de>
2015-11-17 18:59:28 +0100

    Sellcontroller + sell.html changed to show now properly for allauthenticated useres

--- prototypes/prototype1/src/main/java/kickstart/controller/SellController.java
+++ prototypes/prototype1/src/main/java/kickstart/controller/SellController.java
@@ -25,18 +25,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@PreAuthorize("hasRole('ROLE_PCUSTOMER')")
+@PreAuthorize("hasAnyRole('ROLE_PCUSTOMER', 'ROLE_BOSS', 'ROLE_EMPLOYEE')")
 @SessionAttributes("sell")
 public class SellController {
 	
-/*	@RequestMapping(value = "/sell", method = RequestMethod.POST)
-	public String addArticel(@RequestParam("Computertype") ComputerType ComputerType,@RequestParam("Computer") Computer Computer, @RequestParam("number") int number, @ModelAttribute Cart Sell) {
+	private final OrderManager<Order> sellManager;
 	
-		Cart.addOrUpdateItem(ComputerType, Quantity.of(number));
-	} */
+	@Autowired
+	public SellController(OrderManager<Order> sellManager) {
+
+		Assert.notNull(sellManager, "sellManager must not be null!");
+		this.sellManager = sellManager;
+	}
+	
+	@ModelAttribute("sell")
+	public Cart initializeSell() {
+		return new Cart();
+	}
+	@RequestMapping(value = "/sell", method = RequestMethod.POST)
+	public String addSellArticel(@RequestParam("Computer") Computer Computer, int number, @ModelAttribute Cart sell) {
+	
+		int amount = 1;
+		
+		sell.addOrUpdateItem(Computer, Quantity.of(amount));
+		
+		switch (Computer.getType()) {
+		case COMPUTER:
+			return "redirect:computerCatalog";
+		case NOTEBOOK:
+		default:
+			return "redirect:notebookCatalog";
+	}
+	} 
+	
 	@RequestMapping(value = "/sell", method = RequestMethod.GET)
 	public String sellorder() {
 		return "sell";
 	}
 	
+/*	@RequestMapping(value = "/checkout", method = RequestMethod.POST)
+	public String sendrequest(@ModelAttribute Cart sell, @LoggedIn Optional<UserAccount> userAccount) {
+
+		return userAccount.map(account -> {
+
+			Order order = new Order(account, Cash.CASH);
+
+			sell.addItemsTo(order);
+
+			sellManager.payOrder(order);
+			sellManager.completeOrder(order);
+
+			sell.clear();
+
+			return "redirect:/";
+		}).orElse("redirect:/sell");
+	}
+	*/
 }
