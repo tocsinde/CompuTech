eff3d8549f5bfd0e0523caf001702d43bbd69cf6
Stephan Fischer <hallo@tocsin.de>
2015-11-26 18:27:20 +0100

    Designed login formular

--- app/src/main/java/computech/Application.java
+++ app/src/main/java/computech/Application.java
@@ -12,7 +12,7 @@
 
 package computech;
 
-import org.salespointframework.EnableSalespoint;
+/* import org.salespointframework.EnableSalespoint;
 import org.salespointframework.Salespoint;
 import org.salespointframework.SalespointSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
@@ -21,10 +21,20 @@ import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity; */
+
+import org.salespointframework.EnableSalespoint;
+import org.salespointframework.SalespointSecurityConfiguration;
+import org.salespointframework.SalespointWebConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 
 
+
+/*
 @EnableSalespoint
 public class Application {
 
@@ -42,6 +52,42 @@ public class Application {
 
 			http.authorizeRequests().antMatchers("/**").permitAll().and().formLogin().loginProcessingUrl("/login").and()
 					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
+		}
+	}
+} */
+
+
+
+
+@EnableSalespoint
+public class Application {
+
+	private static final String LOGIN_ROUTE = "/login";
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Configuration
+	static class CompuTechWebConfiguration extends SalespointWebConfiguration {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController(LOGIN_ROUTE).setViewName("login");
+		}
+	}
+
+	@Configuration
+	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+
+			http.csrf().disable();
+
+			http.authorizeRequests().antMatchers("/**").permitAll().and().//
+					formLogin().loginPage(LOGIN_ROUTE).loginProcessingUrl(LOGIN_ROUTE).and(). //
+					logout().logoutUrl("/logout").logoutSuccessUrl("/");
 		}
 	}
 }
