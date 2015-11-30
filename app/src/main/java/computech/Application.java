df044c6d45c266ca2db50e0198bc1020982aa021
Stephan Fischer <hallo@tocsin.de>
2015-11-30 16:46:58 +0100

    Removed old stuff from Application.java

--- app/src/main/java/computech/Application.java
+++ app/src/main/java/computech/Application.java
@@ -12,17 +12,6 @@
 
 package computech;
 
-/* import org.salespointframework.EnableSalespoint;
-import org.salespointframework.Salespoint;
-import org.salespointframework.SalespointSecurityConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity; */
-
 import org.salespointframework.EnableSalespoint;
 import org.salespointframework.SalespointSecurityConfiguration;
 import org.salespointframework.SalespointWebConfiguration;
@@ -30,33 +19,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-
-
-
-
-/*
-@EnableSalespoint
-public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	@Configuration
-	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-
-			http.csrf().disable();
-
-			http.authorizeRequests().antMatchers("/**").permitAll().and().formLogin().loginProcessingUrl("/login").and()
-					.logout().logoutUrl("/logout").logoutSuccessUrl("/");
-		}
-	}
-} */
-
-
 
 
 @EnableSalespoint
