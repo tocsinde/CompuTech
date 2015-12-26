7d8d43d0cc21523257c571fcfae355ad9e4210b1
Stephan Fischer <hallo@tocsin.de>
2015-12-26 16:45:45 +0100

    Employee can buy products for business customer, fixes #36

--- app/src/main/java/computech/controller/CartController.java
+++ app/src/main/java/computech/controller/CartController.java
@@ -21,12 +21,15 @@ import org.salespointframework.order.Order;
 import org.salespointframework.order.OrderManager;
 import org.salespointframework.payment.Cash;
 import org.salespointframework.quantity.Quantity;
+import org.salespointframework.useraccount.Role;
 import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManager;
 import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,13 +48,20 @@ class CartController {
 
 
     private static final Quantity NONE = Quantity.of(0);
+    private Optional<UserAccount> userAccount;
+    private ModelMap modelMap;
+    private CustomerRepository customerRepository;
+    private UserAccountManager userAccountManager;
+
     @Autowired
-    public CartController(OrderManager<Order> orderManager, Inventory<InventoryItem> inventory,PartsCatalog partsCatalog) {
+    public CartController(OrderManager<Order> orderManager, Inventory<InventoryItem> inventory,PartsCatalog partsCatalog,CustomerRepository customerRepository,UserAccountManager userAccountManager) {
 
         Assert.notNull(orderManager, "OrderManager must not be null!");
         this.orderManager = orderManager;
         this.inventory = inventory;
         this.partsCatalog= partsCatalog;
+        this.customerRepository=customerRepository;
+        this.userAccountManager=userAccountManager;
 
     }
 
@@ -120,7 +130,16 @@ class CartController {
 
 
     @RequestMapping(value = "/cart", method = RequestMethod.GET)
-    public String basket() {
+    public String basket(@LoggedIn Optional<UserAccount> userAccount, ModelMap modelMap, Model model) {
+
+        for (Role role : userAccount.get().getRoles()) {
+            if(role.getName().equals("ROLE_EMPLOYEE")) {
+                model.addAttribute("employee", userAccount.get());
+                modelMap.addAttribute("customer_list", customerRepository.findAll());
+            }
+        }
+
+
         return "cart";
     }
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
@@ -133,6 +152,26 @@ class CartController {
     public String buy(@ModelAttribute Cart cart, @LoggedIn Optional<UserAccount> userAccount) {
 
         return userAccount.map(account -> {
+
+
+            Order order = new Order(account, Cash.CASH);
+
+            cart.addItemsTo(order);
+
+            orderManager.payOrder(order);
+            orderManager.completeOrder(order);
+
+            cart.clear();
+
+            return "redirect:/";
+        }).orElse("redirect:/cart");
+    }
+
+    @RequestMapping(value = "/checkout_employee", method = RequestMethod.POST)
+    public String buyforbusinesscustomer(@ModelAttribute Cart cart, @RequestParam ("bcustomer") String nickname) {
+
+        Optional <UserAccount> businessCustomer = userAccountManager.findByUsername(nickname);
+        return businessCustomer.map(account -> {
 
 
             Order order = new Order(account, Cash.CASH);
